3a6de7563bf98602118d9b8a95afbf98b1dab4ba
Yuxuan Shui <yshuiv7@gmail.com>
2022-09-03 13:56:34 +0100

    wl_spec: add wayland.xml
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

--- wl_spec/tests/parsing.rs
+++ wl_spec/tests/parsing.rs
@@ -1,5 +1,5 @@
 #[test]
 fn parse() {
-    let file = std::fs::File::open("/usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml").unwrap();
+    let file = std::fs::File::open(concat!(env!("CARGO_MANIFEST_DIR"), "/protocol/wayland.xml")).unwrap();
     eprintln!("{:?}", wl_spec::parse::parse(file).unwrap());
 }

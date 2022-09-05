4f50661fd4194f90cf6b78c67e4f7382af8977db
Yuxuan Shui <yshuiv7@gmail.com>
2022-09-05 23:33:06 +0100

    wl_spec: move protocol/ to top level
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

--- wl_spec/tests/parsing.rs
+++ wl_spec/tests/parsing.rs
@@ -1,5 +1,5 @@
 #[test]
 fn parse() {
-    let file = std::fs::File::open(concat!(env!("CARGO_MANIFEST_DIR"), "/protocol/wayland.xml")).unwrap();
+    let file = std::fs::File::open(concat!(env!("CARGO_WORKSPACE_DIR"), "/protocols/wayland.xml")).unwrap();
     eprintln!("{:?}", wl_spec::parse::parse(file).unwrap());
 }

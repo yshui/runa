14ee9170d18d3ed223f195f97523a13b12ddf91d
Yuxuan Shui <yshuiv7@gmail.com>
2022-12-07 07:31:34 +0000

    apollo: utils: add WeakPtr and Double
    WeakPtr impls Hash and Eq for rc::Weak by pointer comparison.
    Double can be used for reading a shared value without needing to hold a
    borrow of RefCell across await.
    
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

--- apollo/src/utils/mod.rs
+++ apollo/src/utils/mod.rs
@@ -1,4 +1,9 @@
-use std::{hash::Hash, ops::Deref, rc::Rc};
+use std::{
+    cell::RefCell,
+    hash::Hash,
+    ops::Deref,
+    rc::{Rc, Weak},
+};
 pub mod geometry;
 
 /// A wrapper of `Rc` that implements `Hash` and `Eq` by comparing
@@ -50,5 +55,118 @@ impl<T> AsRef<Rc<T>> for RcPtr<T> {
 impl<T> AsMut<Rc<T>> for RcPtr<T> {
     fn as_mut(&mut self) -> &mut Rc<T> {
         &mut self.0
+    }
+}
+
+/// A wrapper of `Weak` that implements `Hash` and `Eq` by comparing
+/// raw pointer addresses.
+#[derive(Debug)]
+pub struct WeakPtr<T>(Weak<T>);
+impl<T> WeakPtr<T> {
+    pub fn new(t: &Rc<T>) -> Self {
+        Self(Rc::downgrade(t))
+    }
+
+    pub fn upgrade(&self) -> Option<Rc<T>> {
+        self.0.upgrade()
+    }
+}
+
+impl<T> PartialEq for WeakPtr<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.ptr_eq(&other.0)
+    }
+}
+
+impl<T> Eq for WeakPtr<T> {}
+
+impl<T> Hash for WeakPtr<T> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        std::ptr::hash(self.0.as_ptr(), state)
+    }
+}
+
+impl<T> From<Weak<T>> for WeakPtr<T> {
+    fn from(value: Weak<T>) -> Self {
+        Self(value)
+    }
+}
+
+/// A double buffered value of T.
+///
+/// This allows concurrent reads and writes to the value. When there are no
+/// readers, changes can be made to the value normally. When readers are
+/// present, writes to this value will be buffered and NOT immediately visible
+/// to the readers.
+///
+/// Only read side can persist across awaits, the write side should never be
+/// borrowed across awaits. Although that's still discouraged, as holding a
+/// reader prevents updates from being visible until all readers are dropped.
+///
+/// When the first reader starts to read, the buffered value is retrieved
+/// (swapped) by using the BufSwap trait. The default behavior is copy out the
+/// updates then CLEAR the buffer. If the write end is being borrowed while the
+/// first reader starts to read, this will panic. But this can be avoided by not
+/// borrowing the write side across awaits.
+///
+/// Using this is better than `tmp = value.borrow().drain().collect()` when you
+/// need to read `value` but can't keep the borrow (e.g. because you can't hold
+/// a borrow across await). Because this doesn't allocate memory every time.
+#[derive(Default)]
+pub struct Double<T> {
+    inner: RefCell<DoubleInner<T>>,
+}
+
+#[derive(Default)]
+struct DoubleInner<T> {
+    front: Rc<T>,
+    back:  Rc<RefCell<T>>,
+}
+
+impl<T: BufSwap> Double<T> {
+    /// Start reading. If this is the first reader, this will retrieve the
+    /// updates from the buffer using the BufSwap trait, usually this means
+    /// the buffer content will be cleared; otherwise, the value retrieved by
+    /// the first reader will be returned.
+    pub fn read(&self) -> Rc<T> {
+        if let Some(read) = self.read_exclusive() {
+            read
+        } else {
+            self.inner.borrow().front.clone()
+        }
+    }
+
+    /// Like [`read`], but makes sure we are the first and only reader,
+    /// returns None otherwise.
+    pub fn read_exclusive(&self) -> Option<Rc<T>> {
+        let mut inner = self.inner.borrow_mut();
+        let back = inner.back.clone();
+        if let Some(front_mut) = Rc::get_mut(&mut inner.front) {
+            // We are the first reader, swap the buffers.
+            front_mut.swap(
+                &mut back
+                    .try_borrow_mut()
+                    .expect("Trying to start read a Double while the write end is borrowed"),
+            );
+            Some(inner.front.clone())
+        } else {
+            None
+        }
+    }
+
+    /// Get a copy of the write end of Double.
+    pub fn write_end(&self) -> Rc<RefCell<T>> {
+        self.inner.borrow_mut().back.clone()
+    }
+}
+
+pub trait BufSwap {
+    fn swap(&mut self, back: &mut Self);
+}
+
+impl<K, V> BufSwap for hashbrown::HashMap<K, V> {
+    fn swap(&mut self, back: &mut Self) {
+        std::mem::swap(self, back);
+        back.clear();
     }
 }
